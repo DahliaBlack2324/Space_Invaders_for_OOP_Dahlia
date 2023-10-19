d29c18a0387fa6d15345439e5b21402ce948518e
TatianaMaksakova <tatzianar@gmail.com>
2023-10-19 17:16:20 +0300

    хоп-хей ла-ла-лей

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 import sys
+from gg import GG
 
+def start_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1600,900))
+    pygame.display.set_caption("?????")
+
+    flag = True
+    gg = GG(screen)
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+        gg.output()
+
+start_game()
